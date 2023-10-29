acb73c6c30f9fe67e225fa60715810a15f708823
Kayh <basedlainpilled@gmail.com>
2023-10-29 12:25:32 -0400

    spin vrm in example

--- examples/load_vrm.rs
+++ examples/load_vrm.rs
@@ -7,18 +7,31 @@ fn main() {
     App::new()
         .add_plugins((DefaultPlugins, VRMPlugin))
         .add_systems(Startup, setup)
+        .add_systems(Update, rotate_vrm)
         .run();
 }
+
+#[derive(Component)]
+struct VRMTag;
 
 fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
     let mut transform = Transform::from_xyz(0.0, -1.0, -4.0);
     transform.rotate_y(PI);
 
-    commands.spawn((SceneBundle {
-        scene: asset_server.load("suzuha.vrm#Scene0"),
-        transform,
-        ..default()
-    },));
+    commands.spawn((
+        SceneBundle {
+            scene: asset_server.load("suzuha.vrm#Scene0"),
+            transform,
+            ..default()
+        },
+        VRMTag,
+    ));
 
     commands.spawn(Camera3dBundle::default());
+}
+
+fn rotate_vrm(time: Res<Time>, mut query: Query<&mut Transform, With<VRMTag>>) {
+    for mut transform in query.iter_mut() {
+        transform.rotate(Quat::from_rotation_y(time.delta_seconds()));
+    }
 }
