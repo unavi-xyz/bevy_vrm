828a4f5def1b8ce2c2e930926c84b2a20ef9c35d
Ryan Butler <thebutlah@gmail.com>
2023-12-25 00:33:09 -0600

    Added RootExtensions field to vrm struct

--- bevy_vrm/src/loader/vrm.rs
+++ bevy_vrm/src/loader/vrm.rs
@@ -2,15 +2,17 @@ use bevy::asset::LoadContext;
 
 use crate::Vrm;
 
-pub fn load_gltf(
-    _vrm: &mut Vrm,
-    gltf: &goth_gltf::Gltf<super::Extensions>,
+pub(crate) fn load_gltf(
+    vrm: &mut Vrm,
+    goth_gltf: &goth_gltf::Gltf<super::Extensions>,
     _load_context: &mut LoadContext,
 ) -> Result<(), Box<dyn std::error::Error>> {
-    let _vrmc_vrm = match &gltf.extensions.vrmc_vrm {
+    let _vrmc_vrm = match &goth_gltf.extensions.vrmc_vrm {
         Some(vrmc_vrm) => vrmc_vrm,
         None => return Err("VRMC_vrm not found".into()),
     };
+
+    vrm.extensions = goth_gltf.extensions.clone();
 
     Ok(())
 }
