965cd5e740c3af997f48d4f1f2ea183d53c4cbe4
Kayh <basedlainpilled@gmail.com>
2024-08-02 12:45:22 -0400

    add RenderLayer docs

--- README.md
+++ README.md
@@ -5,4 +5,12 @@
 [Bevy](https://bevyengine.org/) plugin for loading [VRM](https://vrm.dev/en/) avatars.
 Aims to support both the VRM 0.0 and VRM 1.0 standards.
 
+### Render Layers
+
+[RenderLayers](bevy::render::view::RenderLayers) are used to represent VRM mesh annotations.
+If parts of your loaded VRM are invisible, it is likely because of this!
+
+Check out the [layers] module to see the used values.
+Add the corresponding layer to your camera for it to be rendered.
+
 <!-- cargo-rdme end -->
--- crates/bevy_vrm/src/lib.rs
+++ crates/bevy_vrm/src/lib.rs
@@ -1,5 +1,13 @@
 //! [Bevy](https://bevyengine.org/) plugin for loading [VRM](https://vrm.dev/en/) avatars.
 //! Aims to support both the VRM 0.0 and VRM 1.0 standards.
+//!
+//! ## Render Layers
+//!
+//! [RenderLayers](bevy::render::view::RenderLayers) are used to represent VRM mesh annotations.
+//! If parts of your loaded VRM are invisible, it is likely because of this!
+//!
+//! Check out the [layers] module to see the used values.
+//! Add the corresponding layer to your camera for it to be rendered.
 
 use auto_scene::AutoScene;
 use bevy::{app::PluginGroupBuilder, prelude::*};
