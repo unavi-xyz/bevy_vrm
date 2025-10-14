1d8dccfb801a9628e29d5fd458718b40edd3d72e
Kayh <basedlainpilled@gmail.com>
2025-10-14 08:37:16 -0400

    flake updates

---
+++ crates/vrm_viewer/default.nix
@@ -0,0 +1,85 @@
+_: {
+  perSystem =
+    { pkgs, lib, ... }:
+    let
+      pname = "vrm_viewer";
+
+      buildInputs = pkgs.lib.optionals pkgs.stdenv.isLinux (
+        with pkgs;
+        [
+          alsa-lib
+          libxkbcommon
+          openssl
+          udev
+          vulkan-loader
+          wayland
+          xorg.libX11
+          xorg.libXcursor
+          xorg.libXi
+          xorg.libXrandr
+        ]
+      );
+
+      nativeBuildInputs = pkgs.lib.optionals pkgs.stdenv.isLinux (with pkgs; [ pkg-config ]);
+
+      src = lib.fileset.toSource rec {
+        root = ../..;
+        fileset = lib.fileset.unions [
+          (pkgs.crane.fileset.commonCargoSources root)
+          (lib.fileset.fileFilter (
+            file:
+            lib.any file.hasExt [
+              "html"
+              "wgsl"
+            ]
+          ) root)
+          ../../LICENSE-APACHE
+          ../../LICENSE-MIT
+          ../../assets
+          ../../public
+        ];
+      };
+
+      cargoArgs = {
+        inherit buildInputs;
+        inherit nativeBuildInputs;
+        inherit pname;
+        inherit src;
+
+        runtimeDependencies = buildInputs;
+
+        cargoExtraArgs = "-p ${pname}";
+        strictDeps = true;
+      };
+
+      cargoArtifacts = pkgs.crane.buildDepsOnly cargoArgs;
+    in
+    {
+      checks = {
+        "${pname}-doc" = pkgs.crane.cargoDoc (cargoArgs // { inherit cargoArtifacts; });
+        "${pname}-doctest" = pkgs.crane.cargoDocTest (cargoArgs // { inherit cargoArtifacts; });
+        "${pname}-nextest" = pkgs.crane.cargoNextest (
+          cargoArgs
+          // {
+            inherit cargoArtifacts;
+            cargoExtraArgs = cargoArgs.cargoExtraArgs + " --no-tests pass";
+          }
+        );
+      };
+
+      packages."${pname}" = pkgs.crane.buildPackage (
+        cargoArgs
+        // {
+          inherit cargoArtifacts;
+          doCheck = false;
+
+          postInstall = ''
+            mv $out/bin/* $out
+            rm -r $out/bin
+            cp LICENSE-APACHE $out
+            cp LICENSE-MIT $out
+          '';
+        }
+      );
+    };
+}
--- flake.nix
+++ flake.nix
@@ -1,236 +1,110 @@
 {
   inputs = {
-    crane.url = "github:ipetkov/crane";
-    flake-utils.url = "github:numtide/flake-utils";
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    rust-overlay = {
-      url = "github:oxalica/rust-overlay";
-      inputs = {
-        nixpkgs.follows = "nixpkgs";
-      };
+    flake-parts = {
+      inputs.nixpkgs-lib.follows = "nixpkgs";
+      url = "github:hercules-ci/flake-parts";
     };
-    treefmt-nix.url = "github:numtide/treefmt-nix";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    systems.url = "github:nix-systems/default";
 
+    # Rust
+    advisory-db = {
+      url = "github:rustsec/advisory-db";
+      flake = false;
+    };
+    crane.url = "github:ipetkov/crane";
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    # Other
+    treefmt-nix.url = "github:numtide/treefmt-nix";
   };
 
   outputs =
-    {
-      crane,
-      flake-utils,
-      nixpkgs,
-      rust-overlay,
-      self,
-      treefmt-nix,
-
-      ...
-    }:
-    flake-utils.lib.eachDefaultSystem (
-      localSystem:
-      let
-        pkgs = import nixpkgs {
-          inherit localSystem;
-          overlays = [ (import rust-overlay) ];
-        };
-
-        inherit (pkgs) lib;
-
-        rustToolchain = pkgs.pkgsBuildHost.rust-bin.stable.latest.default.override {
-          targets = [ "wasm32-unknown-unknown" ];
-        };
-
-        craneLib = (crane.mkLib pkgs).overrideToolchain rustToolchain;
-
-        commonArgs = {
-          src = lib.fileset.toSource rec {
-            root = ./.;
-            fileset = lib.fileset.unions [
-              (craneLib.fileset.commonCargoSources root)
-              (lib.fileset.fileFilter (
-                file:
-                lib.any file.hasExt [
-                  "html"
-                  "wgsl"
-                ]
-              ) root)
-              ./assets
-              ./public
-            ];
-          };
-
-          strictDeps = true;
-
-          buildInputs =
-            lib.optionals pkgs.stdenv.isLinux (
-              with pkgs;
-              [
-                alsa-lib
-                alsa-lib.dev
-                libxkbcommon
-                udev
-                vulkan-loader
-                wayland
-                xorg.libX11
-                xorg.libXcursor
-                xorg.libXi
-                xorg.libXrandr
-              ]
-            )
-            ++ lib.optionals pkgs.stdenv.isDarwin (with pkgs; [ darwin.apple_sdk.frameworks.Cocoa ]);
-
-          nativeBuildInputs =
-            (with pkgs; [
-              binaryen
-              pkg-config
-              trunk
-              wasm-bindgen-cli
-
-            ])
-            ++ lib.optionals (!pkgs.stdenv.isDarwin) (
-              with pkgs;
-              [
-                alsa-lib
-                alsa-lib.dev
-              ]
-            );
-        };
-
-        commonShell = {
-          checks = self.checks.${localSystem};
-          packages = with pkgs; [
-            cargo-edit
-            cargo-rdme
-            cargo-release
-            cargo-watch
-            cargo-workspaces
-          ];
-
-          LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath commonArgs.buildInputs;
-        };
-
-        cargoArtifacts = craneLib.buildDepsOnly (commonArgs // { pname = "deps"; });
-
-        cargoClippy = craneLib.cargoClippy (
-          commonArgs
-          // {
-            inherit cargoArtifacts;
-            pname = "clippy";
-          }
-        );
-
-        cargoDoc = craneLib.cargoDoc (
-          commonArgs
-          // {
-            inherit cargoArtifacts;
-            pname = "doc";
-          }
-        );
-
-        bevy_shader_mtoon = craneLib.buildPackage (
-          commonArgs
-          // {
-            inherit cargoArtifacts;
-            pname = "bevy_shader_mtoon";
-            cargoExtraArgs = "-p bevy_shader_mtoon";
-          }
-        );
-
-        bevy_vrm = craneLib.buildPackage (
-          commonArgs
-          // {
-            inherit cargoArtifacts;
-            pname = "bevy_vrm";
-            cargoExtraArgs = "-p bevy_vrm";
-          }
-        );
-
-        gltf_kun_vrm = craneLib.buildPackage (
-          commonArgs
-          // {
-            inherit cargoArtifacts;
-            pname = "gltf_kun_vrm";
-            cargoExtraArgs = "-p gltf_kun_vrm";
-          }
-        );
-
-        serde_vrm = craneLib.buildPackage (
-          commonArgs
-          // {
-            inherit cargoArtifacts;
-            pname = "serde_vrm";
-            cargoExtraArgs = "-p serde_vrm";
-          }
-        );
-
-        vrm_viewer = craneLib.buildPackage (
-          commonArgs
-          // {
-            inherit cargoArtifacts;
-
-            pname = "vrm_viewer";
-            cargoExtraArgs = "-p vrm_viewer";
-            postInstall = ''
-              cp -r assets $out/bin/
-            '';
-          }
-        );
-
-        vrm_viewer_web = craneLib.buildTrunkPackage (
-          commonArgs
-          // {
-            pname = "vrm_viewer_web";
-            cargoExtraArgs = "-p vrm_viewer";
-
-            inherit (pkgs) wasm-bindgen-cli;
-          }
-        );
-
-        treefmtEval = treefmt-nix.lib.evalModule pkgs ./treefmt.nix;
-      in
+    inputs@{ flake-parts, systems, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } (
+      { ... }:
       {
-        formatter = treefmtEval.config.build.wrapper;
+        systems = import systems;
 
-        checks = {
-          inherit
-            bevy_vrm
-            bevy_shader_mtoon
-            gltf_kun_vrm
-            serde_vrm
-            vrm_viewer
-            vrm_viewer_web
-            cargoClippy
-            cargoDoc
-            ;
-        };
+        imports = [
+          inputs.treefmt-nix.flakeModule
+          ./crates/vrm_viewer
+        ];
 
-        apps = {
-          vrm_viewer = flake-utils.lib.mkApp { drv = vrm_viewer; };
-          vrm_viewer_web = flake-utils.lib.mkApp {
-            drv = pkgs.writeShellScriptBin "vrm_viewer_web" ''
-              ${pkgs.python3Minimal}/bin/python3 -m http.server --directory ${
-                self.packages.${localSystem}.vrm_viewer_web
-              } 3000
-            '';
+        perSystem =
+          {
+            config,
+            lib,
+            pkgs,
+            system,
+            ...
+          }:
+          {
+            _module.args.pkgs = import inputs.nixpkgs {
+              inherit system;
+              overlays = [
+                inputs.fenix.overlays.default
+
+                (
+                  self: _:
+                  let
+                    nightly = self.fenix.complete.toolchain;
+                  in
+                  {
+                    crane = (inputs.crane.mkLib self).overrideToolchain nightly;
+                  }
+                )
+              ];
+            };
+
+            checks = {
+              audit = pkgs.crane.cargoAudit {
+                inherit (inputs) advisory-db;
+                src = ./.;
+                pname = "bevy_vrm";
+              };
+            };
+
+            treefmt.programs = {
+              actionlint.enable = true;
+              deadnix.enable = true;
+              mdformat.enable = true;
+              nixfmt = {
+                enable = true;
+                strict = true;
+              };
+              rustfmt.enable = true;
+              statix.enable = true;
+              taplo.enable = true;
+              yamlfmt.enable = true;
+            };
+
+            devShells.default = pkgs.crane.devShell {
+              packages =
+                (with pkgs; [
+                  cargo-edit
+                  cargo-machete
+                  cargo-nextest
+                  cargo-release
+                  cargo-workspaces
+                ])
+                ++ (
+                  config.packages
+                  |> lib.attrValues
+                  |> lib.flip pkgs.lib.forEach (x: x.buildInputs ++ x.nativeBuildInputs)
+                );
+
+              LD_LIBRARY_PATH =
+                config.packages
+                |> lib.attrValues
+                |> lib.flip pkgs.lib.forEach (x: x.runtimeDependencies)
+                |> lib.concatLists
+                |> lib.makeLibraryPath;
+            };
           };
-        };
-
-        packages = {
-          inherit bevy_shader_mtoon;
-          inherit bevy_vrm;
-          inherit gltf_kun_vrm;
-          inherit serde_vrm;
-          inherit vrm_viewer;
-          inherit vrm_viewer_web;
-
-          default = pkgs.symlinkJoin {
-            name = "viewer";
-            paths = [
-              vrm_viewer
-              vrm_viewer_web
-            ];
-          };
-        };
-
-        devShells.default = craneLib.devShell commonShell;
       }
     );
 }
